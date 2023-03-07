033614f4dd588a49965a994e2a639d5317fb831f
artemnadosha <artemnadosha@gmail.com>
2023-03-07 17:10:38 +0200

    add methods in class CreateVisit

--- src/js/clases/CreateVisit/CreateVisit.js
+++ src/js/clases/CreateVisit/CreateVisit.js
@@ -7,21 +7,24 @@ export class CreateVisit {
     }
 
     renderElement() {
-        const render = inputDefaultVisit.map(item => {
-            if (item.type === 'dropdown') {
-                return item.renderContent(item);
-            } else {
-                return item.renderContent(item);
-            }
-        }).join('');
-        return `<form id="form">${render}</form>`;
-    }
-
-    changeDoctor(value) {
-        this.doctor = value;
+        const render = inputDefaultVisit
+            .map(item => {
+                if (item.type === 'dropdown') {
+                    return item.renderContent(item);
+                } else {
+                    return item.renderContent(item);
+                }
+            })
+            .join('');
+        return `<form id="form-create-visit">${render}</form>`;
     }
 
     renderInputDoctor(value) {
+        const textArea = document.querySelector('textarea');
+        const doctorInputs = document.querySelector('.doctor-inputs');
+
+        !!doctorInputs && doctorInputs.remove();
+
         const newInput = inputDoctorVisit.map(item => {
             if (item.value === value) {
                 return item.content.map(content => {
@@ -33,8 +36,22 @@ export class CreateVisit {
                 });
             }
         });
-        const findingInputContent = newInput.find(item => !!item).join('');
+        const findingInputContent = newInput?.find(item => !!item)?.join('');
 
-        return `<div class="doctor-inputs">${!!findingInputContent && findingInputContent}</div> `;
+        const newHtmlDoctorInput = !!findingInputContent ? `<div class="doctor-inputs">${findingInputContent}</div> ` : '';
+
+        textArea.insertAdjacentHTML('beforebegin', newHtmlDoctorInput);
+    }
+
+    observerChooseDoctor() {
+        const doctorInputs = document.querySelector('#choose-doctor');
+        doctorInputs.addEventListener('change', e => {
+            this.doctor = e.target.value;
+        });
+    }
+
+    removeCreateVisitForm() {
+        const form = document.querySelector('#form-create-visit');
+        form.remove();
     }
 }

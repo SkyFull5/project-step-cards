8e04d374742664e16f790d2e7c718ada89e8a627
artemnadosha <artemnadosha@gmail.com>
2023-03-08 17:02:24 +0200

    bug fix

--- src/js/UI/input/input.js
+++ src/js/UI/input/input.js
@@ -1,1 +1,2 @@
-export const input = ({ type, placeholder, name, value }) => `<input type='${type}' placeholder='${placeholder}' name='${name}' value='${!!value ? value : ''}'>`;
+export const input = ({ type, placeholder, name, value }) => `
+<input type="${type}" placeholder="${placeholder}" name="${name}" value="${!!value ? value : ''}">`;
